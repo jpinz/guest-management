2e1865c265ebbcf42b1041b2f9965f8f0af204db
Julian Pinzer <julian.pinzer@gmail.com>
2018-03-19 09:37:21 -0400

    Hopefully Fixed a CSS bug

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var history = require('connect-history-api-fallback');
 let app = express();
 app.use(history());
 
-app.use(serveStatic(__dirname + "/dist"));
+app.use(serveStatic(__dirname + "/dist/"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
