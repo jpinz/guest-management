de421fc464cd02b40eea05672de2747aa44e7b7d
Julian <julian.pinzer@gmail.com>
2022-02-09 22:49:09 -0500

    Initial Commit, working on it.

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/navigation/Navbar.vue';
+import Login from './components/auth/Login.vue';
+</script>
+
+<template>
+  <Navbar />
+  <Login />
+</template>
