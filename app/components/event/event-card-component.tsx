ea043f0614cb883ca45ff3b74c5a56973ab7d349
Julian <julian@jpinzer.me>
2023-05-01 22:28:01 -0400

    Fix issue where dashboard link wouldnt go to right page.

--- app/components/event/event-card-component.tsx
+++ app/components/event/event-card-component.tsx
@@ -5,7 +5,7 @@ export function EventCardComponent(props: { event: Event, guestCount: number }) 
   const event = props.event;
 
   return (
-    <NavLink to={`/events/${event.organizationId}/event/${event.id}`}>
+    <NavLink to={`/events/${event.organizationId}/event/${event.id}/guests`}>
       <div className="divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow">
         <div className="px-4 py-5 sm:px-6">
           <span>{event.title}</span>
